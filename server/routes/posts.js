042f574ee11e7ac5c3f961bef11f24e22f16935b
Yinusa <yinusaamusa137@gmail.com>
2022-10-03 18:46:02 -0500

    built basic page

---
+++ server/routes/posts.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { getPosts, createPost } from '../controllers/posts.js'; 
+
+const router = express.Router();
+
+router.get('/',getPosts);
+router.get('/',getPosts);
+
+
+// get request method the has a request and sends a response
+
+//localhost:5000/posts
+
+
+export default router;

d591936b92fef759c1bfe269ee2042c7920b2c09
Yinusa <yinusaamusa137@gmail.com>
2022-12-04 20:01:28 -0600

    more backend and oauth bugfix

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,30 @@
+import {AUTH } from './constants';
+import * as api from '../api';
+
+//dispatch -> action -> api request -> reducer
+export const signin = (formData, history) => async (dispatch) => {
+
+    try{
+        const {data} = await api.signIn(formData);
+        console.log("in our Actions", data)
+        dispatch({type: AUTH, data});
+        history.push('/')
+    }
+    catch(error){
+        console.log(error)
+    }
+};
+
+export const signup = (formData,history) => async (dispatch) => {
+
+    try{
+        const {data} = await api.signUp(formData);
+        dispatch({type: AUTH, data});
+        history.push('/')
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
-
+import userRoutes from './routes/user.js';
 
 const app = express(); // initialize our app backend
 dotenv.config();
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 
 app.use('/posts', postRoutes); // every route will start with posts
-
+app.use('/user',userRoutes)
   
 const PORT = process.env.PORT || 5000; // will deploy to our env port or a local 5000 port
 

251446090ce9d6e2cf47570bc226251aac21b079
Yinusa <yinusaamusa137@gmail.com>
2023-01-13 16:57:05 -0600

    added role to the userSchema, added getUsers and getUser controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,38 @@ import jwt from "jsonwebtoken";
 
 import User from "../models/userModel.js";
 
+
+
+
+export const getUsers = async (req, res) => {
+    try{
+        const allUsers = await User.find();
+        res.status(200).json(allUsers)
+    }
+    catch(error){
+        res.status(500).json({message:"No users"})
+     }
+
+     
+
+}  // can modify this to be find all user with {NAME FROM REQ.BODY}
+
+export const getUser = async (req, res) => {
+    const {id} = req.params
+    try{
+        let thisUser = await User.find({email: id}) // or name or id etc.
+        //await User.findById(id) 
+        
+        res.status(200).json(thisUser)
+    }
+    catch(error){
+        res.status(404).json({message:"user NOT FOUND"})
+    }
+}
+
+
+
+
 export const signin = async(req,res) => {
     const {email, password} = req.body
     
@@ -36,7 +68,7 @@ export const signup = async(req,res) => {
          if(password !== confirmPassword) return res.status(400).json({message:"Passwords dont match"})
          const hashedPassword = await bcrypt.hash(password,12);
 
-         const result = await User.create({email, password:hashedPassword, name:`${firstName}, ${lastName}`})
+         const result = await User.create({email, password:hashedPassword, name:`${firstName} ${lastName}`})
          const token = jwt.sign({
             email:result.email, id: result._id
          }, "test", {expiresIn: "1h"})
